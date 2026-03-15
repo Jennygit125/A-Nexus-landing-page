be5cd9f8b7c740e0e33ba0ee4fbde5a298386fd3
Jennygit125 <159704356+Jennygit125@users.noreply.github.com>
2026-03-15 13:01:28 +0100

    building a simple signup/login page with new skill

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const button = document.getElementsByClassName("first_Button");
+
+function handleClick() {
+  window.location.href = "signup.html"
+}
+
+button.addEventListener("click", handleClick);
